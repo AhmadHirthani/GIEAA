2d606e8f0e5c0d7ca8ab8c9de39525c769a94bac
hertai86 <hertani86@gmail.com>
2019-04-17 12:59:05 +0200

    first ver

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # GIEAA
 Impact of GDP on the educational attainment in Austria
+
+Description
+In this project we will try to find the impact of gross domestic product (GDP) on the educational attainment in Austria.
+Our experiment depends on two datasets. The first one is about GDP in world countries and the second is about the educational attainment in world countries.
+The results of project exhibited in csv file and also in visualization manner.
+
+Installation
+No installation needed. You only must have Python installed on your OS and any IDE that support Python. If you would like to use updated versions of datasets you can download them and store in main folder of the project.
+
+
+Support
+If you need any support you can contact Ahmad Alhirthani by Email using hertani86@gmail.com
+
+
+Contributing
+I welcome your contribution on this project. If you have a contribution you can ask by email for joining this project contributors.
+
+
--- main.py
+++ main.py
@@ -2,93 +2,91 @@ import csv
 import os
 import matplotlib.pyplot as plt
 from mpl_toolkits.axisartist.parasite_axes import HostAxes, ParasiteAxes
-import json as simplejson
 
-gdp_list={}
-edu_list={}
-interval=0
-factor=2e10
+gdp_list = {}
+edu_list = {}
+interval = 0
+normalize_factor = 2e10
 
 firstDSpath = os.getcwd() + "\input\GDP by Country.csv"
 secondDSpath = os.getcwd() + "\input\BL2013_MF1599_v2.2.csv"
 results_path = os.getcwd() + "\output\_results.csv"
 
-with open(firstDSpath,'r') as gdp_csv_file:
-    gdp_csv_reader=csv.DictReader(gdp_csv_file)
+with open(firstDSpath, 'r') as gdp_csv_file:
+    gdp_csv_reader = csv.DictReader(gdp_csv_file)
     for line in gdp_csv_reader:
-        if line['ï»¿"Country Name"']=="Austria":
+        if line['\ufeff"Country Name"'] == "Austria":
             for count in range(1961, 1966):
                 endofinterval = 1965
                 interval = interval + float(line['' + str(count) + ''])
-                gdp_list[endofinterval]=interval / factor
+                gdp_list[endofinterval] = interval / normalize_factor
             interval = 0
-
 
             for count in range(1966, 1971):
                 endofinterval = 1970
                 interval = interval + float(line['' + str(count) + ''])
-                gdp_list[endofinterval]=interval /factor
+                gdp_list[endofinterval] = interval / normalize_factor
             interval = 0
 
             for count in range(1971, 1976):
-                endofinterval=1975
-                interval=interval+float(line['' + str(count)+''])
-                gdp_list[endofinterval]=interval / factor
-            interval=0
+                endofinterval = 1975
+                interval = interval + float(line['' + str(count) + ''])
+                gdp_list[endofinterval] = interval / normalize_factor
+            interval = 0
 
             for count in range(1976, 1981):
-                endofinterval=1980
-                interval=interval+float(line['' + str(count)+''])
-                gdp_list[endofinterval]=interval / factor
+                endofinterval = 1980
+                interval = interval + float(line['' + str(count) + ''])
+                gdp_list[endofinterval] = interval / normalize_factor
             interval = 0
             for count in range(1981, 1986):
-                endofinterval=1985
-                interval=interval+float(line['' + str(count)+''])
-                gdp_list[endofinterval]=interval /factor
+                endofinterval = 1985
+                interval = interval + float(line['' + str(count) + ''])
+                gdp_list[endofinterval] = interval / normalize_factor
             interval = 0
             for count in range(1986, 1991):
-                endofinterval=1990
-                interval=interval+float(line['' + str(count)+''])
-                gdp_list[endofinterval]=interval /factor
+                endofinterval = 1990
+                interval = interval + float(line['' + str(count) + ''])
+                gdp_list[endofinterval] = interval / normalize_factor
             interval = 0
             for count in range(1991, 1996):
-                endofinterval=1995
-                interval=interval+float(line['' + str(count)+''])
-                gdp_list[endofinterval]=interval /factor
+                endofinterval = 1995
+                interval = interval + float(line['' + str(count) + ''])
+                gdp_list[endofinterval] = interval / normalize_factor
             interval = 0
             for count in range(1996, 2001):
-                endofinterval=2000
-                interval=interval+float(line['' + str(count)+''])
-                gdp_list[endofinterval]=interval /factor
+                endofinterval = 2000
+                interval = interval + float(line['' + str(count) + ''])
+                gdp_list[endofinterval] = interval / normalize_factor
             interval = 0
             for count in range(2001, 2006):
-                endofinterval=2005
-                interval=interval+float(line['' + str(count)+''])
-                gdp_list[endofinterval]=interval /factor
+                endofinterval = 2005
+                interval = interval + float(line['' + str(count) + ''])
+                gdp_list[endofinterval] = interval / normalize_factor
             interval = 0
             for count in range(2006, 2011):
-                endofinterval=2010
-                interval=interval+float(line['' + str(count)+''])
-                gdp_list[endofinterval]=interval /factor
+                endofinterval = 2010
+                interval = interval + float(line['' + str(count) + ''])
+                gdp_list[endofinterval] = interval / normalize_factor
 
-    print(gdp_list)
 
 with open(secondDSpath, 'r') as edu_csv_file:
     edu_csv_reader = csv.DictReader(edu_csv_file)
     for line in edu_csv_reader:
         for c in range(1965, 2011):
             if line['country'] == "Austria" and '' + str(c) + '' == line['year']:
-                edu_list[c]= float(line['lhc']) / float(line['lh'])*100
+                edu_list[c] = float(line['lhc']) / float(line['lh']) * 100
 
-names=list(edu_list.keys())
-gdp_values=list(gdp_list.values())
-edu_values=list(edu_list.values())
-f = open(results_path , 'w')
-simplejson.dump(names, f)
-f.write('\n')
-simplejson.dump( gdp_values,f)
-f.write('\n')
-simplejson.dump( edu_values,  f)
+names = list(edu_list.keys())
+gdp_values = list(gdp_list.values())
+edu_values = list(edu_list.values())
+
+f = open(results_path, 'w')
+i = 0
+f.write("year_intrval,gdp_amount,edu_att_rate \n")
+while i < len(gdp_values):
+    f.write(str(names[i]) + "," + str(gdp_values[i]) + "," + str(edu_values[i]) + "\n")
+    i += 1
 f.close()
 
 fig = plt.figure(1)
@@ -106,20 +104,17 @@ par1.set_ylabel("Temperature")
 par1.axis["right"].major_ticklabels.set_visible(True)
 par1.axis["right"].label.set_visible(True)
 
-
-
 fig.add_axes(host)
 
-host.set_xlim(1975,2010)
+host.set_xlim(1975, 2010)
 host.set_ylim(0, 100)
 
 host.set_xlabel("GDP and Success rate")
 host.set_ylabel("GDP per five years (*2*10^10")
 par1.set_ylabel("Success Rate (%)")
 
-
 p1, = host.plot(names, gdp_values, label="GDP")
-p2, = par1.plot(names,edu_values , label="Success Rate")
+p2, = par1.plot(names, edu_values, label="Success Rate")
 
 par1.set_ylim(0, 100)
 
